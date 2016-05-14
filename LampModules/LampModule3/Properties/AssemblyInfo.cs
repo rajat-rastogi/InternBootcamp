79509224a14923b0bfc7a74e88b7e6ade1c8a984
Rajat Rastogi <rajatras@microsoft.com>
2016-05-13 17:20:21 -0700

    Renaming fix

--- LampModules/LampModule3/Properties/AssemblyInfo.cs
+++ LampModules/LampModule3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LampModule2")]
+[assembly: AssemblyTitle("LampModule3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LampModule2")]
+[assembly: AssemblyProduct("LampModule3")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
